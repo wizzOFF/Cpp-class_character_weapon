bc95830d16051a4cf32a65cfa6db55dcbbfd15e6
WizzOFF <maher_off@proton.me>
2025-04-17 21:30:35 +0200

    Guerrier.hpp: notion of inheritance > Guerrier class inherits from Personnage class

---
+++ Guerrier.hpp
@@ -0,0 +1,16 @@
+
+#ifndef DEF_GUERRIER
+#define DEF_GUERRIER
+ 
+#include <iostream>
+#include <string>
+#include "Personnage.hpp"
+//Ne pas oublier d'inclure Personnage.hpp pour pouvoir en hériter !
+ 
+class Guerrier : public Personnage
+//Signifie : créer une classe Guerrier qui hérite de la classe Personnage
+{
+ 
+};
+ 
+#endif
